c1a90e795664832887fcf51827d13664d537eeea
Joumana <joumanasalahedin@gmail.com>
2023-12-02 18:28:13 +0100

    task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,14 @@
 # installs flask from pip3
 
+package {'werkzeug':
+  ensure   => '2.0',
+  name     => 'werkzeug',
+  provider => 'pip3',
+}
+
 package {'flask':
   ensure   => '2.1.0',
   name     => 'flask',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['werkzeug']
 }
