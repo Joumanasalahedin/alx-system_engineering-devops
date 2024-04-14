f7bdb4b1814fe5e2d7aa8394d5d2f360eea180fd
Joumana <joumanasalahedin@gmail.com>
2024-04-14 18:27:25 +0200

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,21 +4,18 @@ from requests import get
 
 
 def top_ten(subreddit):
-    """Queries Reddit API and prints the titles
-    of the first 10 non-stickied hot posts in a subreddit."""
+    """Queries Reddit API and prints the titles of the first 10 non-stickied hot posts in a subreddit."""
     if not subreddit or not isinstance(subreddit, str):
         print('Invalid subreddit')
         return
 
     api_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     user_agent = {'User-agent': 'my-app/0.0.1'}
-    # Request more posts to account for potential stickied posts
     params = {'limit': 20}
 
     response = get(api_url, headers=user_agent,
                    params=params, allow_redirects=False)
     if response.status_code != 200:
-        print(f"Failed to get data: status code {response.status_code}")
         return
 
     try:
